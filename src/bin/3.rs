6dee1b1ac0b1345b84369ade61cb5d927d226997
Antony Deepak Thomas <antonydeepak@gmail.com>
2024-12-15 18:31:05 -0800

    3 updated

--- src/bin/3.rs
+++ src/bin/3.rs
@@ -16,30 +16,42 @@ fn p1(input: &Vec<String>) -> Result<i64, Box<dyn Error>> {
 }
 
 fn p2(input: &Vec<String>) -> Result<i64, Box<dyn Error>> {
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)").unwrap();
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)")?;
     input
         .iter()
         .flat_map(|x| re.captures_iter(x))
-        .scan(true, |enables, c| match c.get(0) {
-            Some(m) => match m.as_str() {
-                "don't()" => {
-                    *enables = false;
-                    Some(Ok(0))
-                }
-                "do()" => {
-                    *enables = true;
-                    Some(Ok(0))
-                }
-                _ => {
-                    if *enables {
-                        Some(Ok(c.get(1).unwrap().as_str().parse::<i64>().unwrap()
-                            * c.get(2).unwrap().as_str().parse::<i64>().unwrap()))
-                    } else {
-                        Some(Ok(0))
+        .scan(true, |enables, c| {
+            let r: Result<i64, Box<dyn Error>> = (|| {
+                let m = c.get(0).ok_or("No match found")?;
+                match m.as_str() {
+                    "don't()" => {
+                        *enables = false;
+                        Ok(0)
+                    }
+                    "do()" => {
+                        *enables = true;
+                        Ok(0)
+                    }
+                    _ => {
+                        if *enables {
+                            let a = c
+                                .get(1)
+                                .ok_or("Missing first group")?
+                                .as_str()
+                                .parse::<i64>()?;
+                            let b = c
+                                .get(2)
+                                .ok_or("Missing second group")?
+                                .as_str()
+                                .parse::<i64>()?;
+                            Ok(a * b)
+                        } else {
+                            Ok(0)
+                        }
                     }
                 }
-            },
-            None => Some(Err(Box::from("No match"))),
+            })();
+            Some(r)
         })
         .sum()
 }
